04b75dfe8d83bd1223b62f2a7da901cee0208402
Janghun Lee <74360958+bh2980@users.noreply.github.com>
2022-06-25 13:07:09 +0900

    [Silver III] Title: 계단 오르기, Time: 80 ms, Memory: 30864 KB -BaekjoonHub

---
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.py
@@ -0,0 +1,19 @@
+if __name__ == '__main__':
+  n = int(input())
+  stairs = [int(input()) for _ in range(n)]
+  
+  score_arr = [[0, 0] for _ in range(n + 1)]
+
+  score_arr[1][1] = stairs[0]
+
+  if n > 1:
+    score_arr[2][0] = stairs[1]
+    score_arr[2][1] = stairs[0] + stairs[1]
+
+  if n > 2:
+    for i in range(3, n+1):
+      score_arr[i][0] = max(score_arr[i-2][0], score_arr[i-2][1]) + stairs[i-1]
+      
+      score_arr[i][1] = score_arr[i-1][0] + stairs[i-1]
+      
+  print(max(score_arr[n][0], score_arr[n][1]))
