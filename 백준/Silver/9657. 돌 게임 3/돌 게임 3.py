a84891b8026b9fb5e50d0feb7270a51d39797390
Janghun Lee <74360958+bh2980@users.noreply.github.com>
2022-07-10 00:19:50 +0900

    [Silver III] Title: 돌 게임 3, Time: 76 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/9657. 돌 게임 3/돌 게임 3.py
+++ 백준/Silver/9657. 돌 게임 3/돌 게임 3.py
@@ -1,15 +1,18 @@
 n = int(input())
 
-winner = ['SK' for _ in range(n+1)]
+FIRST_START = 1
+LAST_START = 2
+
+winner = [FIRST_START for _ in range(n+1)]
 try:
-  winner[2] = 'CY'
+  winner[2] = LAST_START
 except:
   pass
 
 for i in range(5, n+1):
-  if 'CY' in [winner[i-4], winner[i-3], winner[i-1]]:
-    winner[i] = 'SK'
+  if LAST_START in [winner[i-4], winner[i-3], winner[i-1]]:
+    winner[i] = FIRST_START
   else:
-    winner[i] = 'CY'
+    winner[i] = LAST_START
 
-print(winner[n])
+print('CY' if winner[n] == LAST_START else 'SK')
