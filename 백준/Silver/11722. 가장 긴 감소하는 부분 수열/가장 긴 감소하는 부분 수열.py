c7ab3cd36cd109424277ac5d53fb5f26d7c0b414
Janghun Lee <74360958+bh2980@users.noreply.github.com>
2023-04-15 05:32:50 +0900

    [Silver II] Title: 가장 긴 감소하는 부분 수열, Time: 100 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/11722. 가장 긴 감소하는 부분 수열/가장 긴 감소하는 부분 수열.py
@@ -0,0 +1,18 @@
+def solution(n, num_list):
+    # 이전에 있는 것들 중 나보다 크면서 최대 길이가 가장 큰 것
+
+    dp = [1 for _ in range(n)]
+
+    for i in range(1, n):
+        # 나보다 작은 idx 중에
+        # 가장 긴 길이를 찾아서 + 1
+        for j in range(i):
+            if num_list[j] > num_list[i]:
+                dp[i] = max(dp[i], dp[j] + 1)
+
+    return max(dp)
+
+n = int(input())
+num_list = list(map(int, input().split()))
+
+print(solution(n, num_list))
