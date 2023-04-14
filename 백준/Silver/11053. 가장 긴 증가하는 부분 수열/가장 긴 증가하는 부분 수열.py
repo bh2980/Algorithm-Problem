ea91593cd75ed6366734e533ae13c92d045012f4
Janghun Lee <74360958+bh2980@users.noreply.github.com>
2023-04-15 04:38:13 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 96 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.py
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.py
@@ -1,21 +1,16 @@
 def solution(n, num_list):
     # 이전에 있는 것들 중 나보다 작으면서 최대 길이가 가장 큰 것
 
-    dp = dict([[i, 1] for i in range(n)])
-
-    max_length = 1
+    dp = [1 for _ in range(n)]
 
     for i in range(1, n):
-        # 최대 길이가 가장 큰 순으로 정렬
-        # 나보다 작은 것을 찾음
-        for idx, length in sorted(list(dp.items()), key = lambda x: -x[1]):
-            if idx < i and num_list[idx] < num_list[i]:
-                dp[i] = length + 1
+        # 나보다 작은 idx 중에
+        # 가장 긴 길이를 찾아서 + 1
+        for j in range(i):
+            if num_list[j] < num_list[i]:
+                dp[i] = max(dp[i], dp[j] + 1)
 
-                max_length = max(max_length, dp[i])
-                break
-
-    return max_length
+    return max(dp)
 
 n = int(input())
 num_list = list(map(int, input().split()))
