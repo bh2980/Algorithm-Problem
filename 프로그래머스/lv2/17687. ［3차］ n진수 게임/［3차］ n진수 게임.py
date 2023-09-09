248927862ff63393dda8537c94c9f9f366264279
Janghun Lee <74360958+bh2980@users.noreply.github.com>
2023-09-10 01:02:46 +0900

    [level 2] Title: [3차] n진수 게임, Time: 11.23 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.py
@@ -0,0 +1,48 @@
+def nNumber(n, number):
+    num_string = ''
+    
+    if number == 0:
+        return '0'
+    
+    while number != 0:
+        mok, rest = divmod(number, n)
+
+        number = mok
+        
+        if rest == 10:
+            rest = 'A'
+        elif rest == 11:
+            rest = 'B'
+        elif rest == 12:
+            rest = 'C'
+        elif rest == 13:
+            rest = 'D'
+        elif rest == 14:
+            rest = 'E'
+        elif rest == 15:
+            rest = 'F'
+            
+        num_string = str(rest) + num_string
+        
+    return num_string
+
+def solution(n, t, m, p):
+    #특정 진법의 t개의 수를 string으로 잇습니다.
+    #다 이은 수에서 p번째부터 m개마다 t개를 가져옵니다.
+    #2진법부터 16진법까지 모두 구현해야하는거야?
+    
+    answer = ''
+    number_string = ''
+    
+    num = 0
+    
+    while len(number_string) <= t * m + p:
+        number_string += nNumber(n, num)
+        num += 1
+        
+    start_idx = p - 1
+    for _ in range(t):
+        answer += number_string[start_idx]
+        start_idx += m
+    
+    return answer
