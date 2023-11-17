cdf2339b820ea40ff9f4335632d9417467888901
Janghun Lee <74360958+bh2980@users.noreply.github.com>
2023-11-17 22:03:36 +0900

    [level 2] Title: 피로도, Time: 0.36 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/87946. 피로도/피로도.js
@@ -0,0 +1,38 @@
+let maxCount = 0;
+
+function exploreDFS(dungeons, k, currentIdx = 0, count = 0, visited = new Set()){
+    // console.log(`현재까지 탐사한 던전 ${count}개 / 현재 피로토 ${k}`)
+    //재귀적으로 for문을 돌려 탐색함.
+    for(let i = 0; i < dungeons.length; i++){
+        if(visited.has(i)) continue;
+        
+        const [minNeed, consume] = dungeons[i];
+        
+        if(k < minNeed) {
+            // console.log(`${i}번째 던전 탐사 불가 / 필요 피로도 ${minNeed}`)
+            continue
+        };
+        
+        // console.log(`${i}번째 던전 탐사 / 소모 피로도 ${consume} / 남은 피로도 ${k - consume}`);
+        k -= consume;
+        visited.add(i);
+        exploreDFS(dungeons, k, i + 1, count + 1, visited);
+        k += consume;
+        visited.delete(i);
+    }
+    //하나를 돌 때마다 count ++;
+    //더 이상 돌 수 있는 던전이 없다면 maxCount 업데이트
+    //만약 모든 던전을 다 돌 수 있는 경우가 나오면 바로 return하는 최적화
+    
+    maxCount = Math.max(count, maxCount);
+    
+    // console.log()
+    
+    return;
+}
+
+function solution(k, dungeons) {
+    exploreDFS(dungeons, k);
+    
+    return maxCount;
+}
